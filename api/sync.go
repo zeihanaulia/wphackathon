752d5683e1cce9956025574ed726544a2de4fa56
Zeihan Aulia <zeihan@warungpintar.co>
2020-11-07 20:28:05 +0700

    implement engine scraping

--- api/sync.go
+++ api/sync.go
@@ -1,10 +1,36 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/zeihanaulia/instagram-scraper/config"
+	"github.com/zeihanaulia/instagram-scraper/internal/engine"
+	"github.com/zeihanaulia/instagram-scraper/internal/engine/ports"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go Version2!</h1>")
+	go func() {
+		engine.Start(config.Configurations{
+			SHEETY_API:         os.Getenv("SHEETY_API"),
+			INSTAGRAM_USERNAME: os.Getenv("INSTAGRAM_USERNAME"),
+			INSTAGRAM_PASSWORD: os.Getenv("INSTAGRAM_PASSWORD"),
+		})
+	}()
+
+	response := ports.ResponseHashTag{
+		SyncAt: time.Now().Format(time.RFC3339),
+		Status: "Requested",
+	}
+	js, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte(js))
 }
